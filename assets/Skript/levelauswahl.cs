a3c38b040121247b659e1a47e6fee42361b03c9a
ChriCh <c.chlebisz@gmx.de>
2017-01-19 18:51:48 +0100

    Levelauswahl
    Level frei schalten wenn man ziel des vorherigen levels erreicht hat

---
+++ assets/Skript/levelauswahl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class levelauswahl : MonoBehaviour {
+
+	public string sceneToload;
+
+	// Use this for initialization
+	void Start () {
+		//Level 1 aktiv
+		PlayerPrefs.SetInt("Szene00", 1);
+
+		if (PlayerPrefs.GetInt (sceneToload.ToString ()) == 1) {
+			//Button aktivieren
+			this.GetComponent<Button>().interactable = true;
+		} else 
+		{
+			//Button deaktivieren
+			this.GetComponent<Button>().interactable = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadLevel(){
+		SceneManager.LoadScene (sceneToload);
+	}
+}
--- assets/Skript/naechstesLevel.cs
+++ assets/Skript/naechstesLevel.cs
@@ -5,14 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class naechstesLevel : MonoBehaviour {
 
-
+	public string sceneToLoad;
 		
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Spieler") 
 		{
 			Debug.Log ("Next Level");
-			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+			LoadLevel ();
+			//SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+		}
+	}
+
+	void LoadLevel()
+	{
+		//nächste Szene laden
+		SceneManager.LoadScene(sceneToLoad);
+		//Szene speichern
+		if (PlayerPrefs.GetInt (sceneToLoad.ToString ()) == 0) {
+			PlayerPrefs.SetInt (sceneToLoad.ToString (), 1);
 		}
 	}
 }
