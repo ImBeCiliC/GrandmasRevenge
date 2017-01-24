57f39fc035d5b1fc71ed997d8de8227e94b27c2f
ChriCh <c.chlebisz@gmx.de>
2017-01-24 14:47:42 +0100

    level3 fertig
    level3 fertig erstellt, wieder mit zusätzlichem roboter zum testen

---
+++ assets/Skript/fallPlattform.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallPlattform : MonoBehaviour {
+
+	private Rigidbody2D rb2dPlattform;
+	public float zeit; 
+	public GameObject platt;
+	// Use this for initialization
+	void Start () {
+		rb2dPlattform = GetComponent<Rigidbody2D> ();
+		platt = this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.collider.tag == "Spieler") 
+		{
+			Debug.Log ("plattform fällt");
+			//StartCoroutine (Fall ());
+			StartCoroutine(Destroy());
+		}
+	}
+	IEnumerator Fall()
+	{
+		//Plattform fallen
+		yield return new WaitForSeconds (zeit);
+		rb2dPlattform.isKinematic = false;
+
+
+	}
+	IEnumerator Destroy()
+	{
+		//Plattform fallen
+		yield return new WaitForSeconds (zeit);
+		Destroy (platt);
+
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "killzone") 
+		{
+			Destroy (platt);
+		}
+
+	}
+}
